23f69d34cbcac3c96668d77b666aa629bb183cd9
Bazell Partridge <elbitapmoC@users.noreply.github.com>
2023-03-29 14:16:30 -0400

    Practices: - Request obj - Response obj - Returning html pages - Routing (basic)

---
+++ 01-node-tut/6-server.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const fs = require("fs");
+
+const port = 3000;
+
+// We have access to two functions, req & res
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  // Set header content type
+  res.setHeader("Content-Type", "text/html");
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  // send html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    }
+    res.end(data);
+  });
+});
+
+// Here we're using our own computer as a host
+// ports - like doors into your computer
+server.listen(port, "localhost", () => {
+  console.log(`🚀 Listening... Port: ${port}`);
+});
